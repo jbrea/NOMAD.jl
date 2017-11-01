4e3462829f10fdaf9bdc0b31874ed1179a28b9d7
Johanni Brea <johannibrea@gmail.com>
2017-11-01 14:09:46 +0100

    retab

--- deps/build.jl
+++ deps/build.jl
@@ -11,26 +11,26 @@ libsgtelib = library_dependency("libsgtelib")
 
 filename = "unix_linux/NOMAD.zip"
 if is_apple()
-	filename = "mac/NOMAD.dmg"
+    filename = "mac/NOMAD.dmg"
 elseif is_windows()
-	filename = "windows/NOMAD_setup.exe"
+    filename = "windows/NOMAD_setup.exe"
 end
 provides(Sources,
-		 URI("https://sourceforge.net/projects/nomad-bb-opt/files/$filename"),
-		 libnomad, unpacked_dir = "nomad.$nomadversion")
+         URI("https://sourceforge.net/projects/nomad-bb-opt/files/$filename"),
+         libnomad, unpacked_dir = "nomad.$nomadversion")
 
 depsdir = BinDeps.depsdir(libnomad)
 installdir = joinpath(depsdir, "src", "nomad", "install")
 libdir = joinpath(depsdir, "src", "nomad", "lib")
 provides(BuildProcess,
-		 (@build_steps begin
-			GetSources(libnomad)
-			`mv src/nomad.$nomadversion src/nomad`
-			@build_steps begin
-				ChangeDirectory(installdir)
-				`bash install.sh`
-			end
-	   	end), [libnomad, libsgtelib], os = :Linux, installed_libpath=libdir)
+         (@build_steps begin
+            GetSources(libnomad)
+            `mv src/nomad.$nomadversion src/nomad`
+            @build_steps begin
+                ChangeDirectory(installdir)
+                `bash install.sh`
+            end
+        end), [libnomad, libsgtelib], os = :Linux, installed_libpath=libdir)
 
 #TODO build for other OS
 
